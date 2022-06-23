fac0ce07257f9503b5804ab19b54d50d4b2e6943
Cristoval Martinez <cristovalthedon@gmail.com>
2022-06-23 12:17:11 -0700

    first commit

---
+++ server.js
@@ -0,0 +1,9 @@
+import express from 'express'
+import bodyParser from 'body-parser'
+import cookieParser from 'cookie-parser'
+import cors from 'cors'
+import helmet from 'helmet'
+
+const app = express()
+
+export default app
